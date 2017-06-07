3720c3ddffee1a832b7c745787b135937b0fb16d
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-06-07 10:52:45 +0200

    Create customer with getters & setters & validateCustomer methods

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,94 @@
+package model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by michal on 07.06.17.
+ */
+public class Customer {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String login;
+	private String password;
+	private LocalDate createDate;
+	private Boolean isActive;
+	private LocalDate lastLogin;
+
+	public Customer(Integer id, String firstName, String lastName, String login, String password, LocalDate createDate, Boolean isActive, LocalDate lastLogin) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.login = login;
+		this.password = password;
+		this.createDate = createDate;
+		this.isActive = isActive;
+		this.lastLogin = lastLogin;
+	}
+
+	public Customer(String firstName, String lastName, String login, String password, LocalDate createDate, Boolean isActive, LocalDate lastLogin) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.login = login;
+		this.password = password;
+		this.createDate = createDate;
+		this.isActive = isActive;
+		this.lastLogin = lastLogin;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+
+	public LocalDate getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(LocalDate lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean active) {
+		isActive = active;
+	}
+
+	public Boolean validateCustomer(String login, String password){
+		if(validateLogin(login) && validatePassword(password)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private Boolean validateLogin(String loginToValidate){
+		if(login.equals(loginToValidate)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private Boolean validatePassword(String passwordToValidate){
+		if(password.equals(passwordToValidate)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
