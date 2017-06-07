0f655b4fe2dc42e907f37e9af1a9a59b7a1b8cd2
Michal P Doniec <r.pr.michal.doniec@gmail.com>
2017-06-08 00:19:48 +0200

    Create JDBCSQLite (connect to db, execute preparedstatements, reset db) & files with sql statements for db creation, mock data insert & drop table

---
+++ src/test/resources/sql/dropTables.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `Customers`;
+DROP TABLE IF EXISTS `AccountStatuses`;
+DROP TABLE IF  EXISTS `AccountTypes`;
+DROP TABLE IF  EXISTS `Accounts`;
+DROP TABLE IF  EXISTS `CardStatuses`;
+DROP TABLE IF  EXISTS `CardTypes`;
+DROP TABLE IF  EXISTS `Cards`;
+DROP TABLE IF  EXISTS `TransactionStatuses`;
+DROP TABLE IF EXISTS `TransactionTypes`;
+DROP TABLE IF  EXISTS `Transactions`;
