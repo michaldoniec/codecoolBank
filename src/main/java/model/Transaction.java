be6efaa3f65ed84f30880bbf5d1df975744190eb
zBIGniew <zbigniew.piotr.tokarski@gmail.com>
2017-06-07 21:46:19 +0200

    Add Transaction class, and TransactionTest class. 100% coverage.

---
+++ src/main/java/model/Transaction.java
@@ -0,0 +1,94 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Transaction {
+    private Integer id;
+    private LocalDate dateOfTransaction;
+    private String transactionTypeName;
+    private String transactionTypeDescription;
+    private long value;
+    private String description;
+    private String transactionStatusName;
+    private String transactionStatusDescription;
+    private Account sourceAccount;
+    private Card sourceCard;
+    private Account destinationAccount;
+
+
+    public Transaction(Integer id, LocalDate dateOfTransaction, String transactionTypeName,
+        String transactionTypeDescription, long value, String description,
+        String transactionStatusName, String transactionStatusDescription, Account sourceAccount,
+        Card sourceCard, Account destinationAccount) {
+        this.id = id;
+        this.dateOfTransaction = dateOfTransaction;
+        this.transactionTypeName = transactionTypeName;
+        this.transactionTypeDescription = transactionTypeDescription;
+        this.value = value;
+        this.description = description;
+        this.transactionStatusName = transactionStatusName;
+        this.transactionStatusDescription = transactionStatusDescription;
+        this.sourceAccount = sourceAccount;
+        this.sourceCard = sourceCard;
+        this.destinationAccount = destinationAccount;
+    }
+
+
+    public Transaction(LocalDate dateOfTransaction, String transactionTypeName,
+        String transactionTypeDescription, long value, String description,
+        String transactionStatusName, String transactionStatusDescription, Account sourceAccount,
+        Card sourceCard, Account destinationAccount) {
+        this.dateOfTransaction = dateOfTransaction;
+        this.transactionTypeName = transactionTypeName;
+        this.transactionTypeDescription = transactionTypeDescription;
+        this.value = value;
+        this.description = description;
+        this.transactionStatusName = transactionStatusName;
+        this.transactionStatusDescription = transactionStatusDescription;
+        this.sourceAccount = sourceAccount;
+        this.sourceCard = sourceCard;
+        this.destinationAccount = destinationAccount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public LocalDate getDateOfTransaction() {
+        return dateOfTransaction;
+    }
+
+    public String getTransactionTypeName() {
+        return transactionTypeName;
+    }
+
+    public String getTransactionTypeDescription() {return transactionTypeDescription;}
+
+    public long getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTransactionStatusName() {
+        return transactionStatusName;
+    }
+
+    public String getTransactionStatusDescription() {
+        return transactionStatusDescription;
+    }
+
+    public Account getSourceAccount() {
+        return sourceAccount;
+    }
+
+    public Card getSourceCard() {
+        return sourceCard;
+    }
+
+    public Account getDestinationAccount() {
+        return destinationAccount;
+    }
+}
